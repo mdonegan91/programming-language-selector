9dffaa57548ce27d886533ecd975b194c972d454
Molly Donegan <mollyrdonegan@gmail.com>
2023-01-13 11:00:53 -0800

    build functions in js

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,28 @@ window.onload = function () {
 
     form.addEventListener("submit", function (event) {
         event.preventDefault();
-    }
+    
         const yourName = document.querySelector("input#name").value;
-        const favoriteTime = document.getElementById("time").value;
-        const favoriteColor = document.getElementById("color").value;
-        const dreamVacation = document.querySelector("input[name='skills']:checked").value;
-        const favSeason = document.getElementById("season").value;
+        const dreamVacation = document.querySelector("input[name='vacation']:checked").value;
+        const favoriteSeason = document.getElementById("season").value;
 
-        let result = "";
-        if (dreamVacation = "Tokyo") + (favoriteTime <= "12:00PM")) {
-            result = "name, " + " you're a morning person with a sense of adventure, you should start your coding journey by learning Javascript.";
+        if (dreamVacation = "Tokyo") {
+            result = yourName + " you're a " + favoriteSeason + " person with a sense of adventure, you should start your coding journey by learning " + "Javascript.";
+        }
+        else if ((dreamVacation = "Aspen") && (favoriteSeason = "Spring" || "Summer")){
+            result = yourName + " you're a " + favoriteSeason + " person with a sense of adventure, you should start your coding journey by learning " + "Javascript.";
+        }
+        else if ((dreamVacation = "Aspen") && (favoriteSeason = "Fall" || "Winter")){
+            result = yourName + " you're a " + favoriteSeason + " person with a sense of adventure, you should start your coding journey by learning " + "C#.";
+        }
+        else if ((dreamVacation = "Fiji") && (favoriteSeason = "Spring" || "Summer")){
+            result = yourName + " you're a " + favoriteSeason + " person with a sense of adventure, you should start your coding journey by learning " + "C#.";
+        }
+        else if ((dreamVacation = "Fiji") && (favoriteSeason = "Fall" || "Winter")){
+            result = yourName + " you're a " + favoriteSeason + " person with a sense of adventure, you should start your coding journey by learning " + "Python.";
         }
 
-        document.getElementById("output").innerText = result;
 
+        document.getElementById("output").innerText = result;
+    });
 }
