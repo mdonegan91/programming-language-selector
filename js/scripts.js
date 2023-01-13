032a1989a17718397ada1cf7d27149ee794170b4
Molly Donegan <mollyrdonegan@gmail.com>
2023-01-13 13:07:36 -0800

    fix broken links

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,35 @@
 window.onload = function () {
-    const form = document.querySelector("form#radio-form");
+    const form = document.querySelector("form#main-form");
 
     form.addEventListener("submit", function (event) {
         event.preventDefault();
-    
+
         const yourName = document.querySelector("input#name").value;
         const dreamVacation = document.querySelector("input[name='vacation']:checked").value;
-        const favoriteSeason = document.getElementById("season").value;
+        const favoriteSeason = document.getElementById("fav-season").value;
+        console.log(yourName);
+        console.log(dreamVacation);
+        console.log(favoriteSeason);
 
-        if (dreamVacation = "Tokyo") {
+        let result = "";
+        if (dreamVacation === "Tokyo") {
             result = yourName + " you're a " + favoriteSeason + " person with a sense of adventure, you should start your coding journey by learning " + "Javascript.";
         }
-        else if ((dreamVacation = "Aspen") && (favoriteSeason = "Spring" || "Summer")){
+        else if ((dreamVacation === "Aspen") && (favoriteSeason === "Spring" || "Summer")) {
             result = yourName + " you're a " + favoriteSeason + " person with a sense of adventure, you should start your coding journey by learning " + "Javascript.";
         }
-        else if ((dreamVacation = "Aspen") && (favoriteSeason = "Fall" || "Winter")){
+        else if ((dreamVacation === "Aspen") && (favoriteSeason === "Fall" || "Winter")) {
             result = yourName + " you're a " + favoriteSeason + " person with a sense of adventure, you should start your coding journey by learning " + "C#.";
         }
-        else if ((dreamVacation = "Fiji") && (favoriteSeason = "Spring" || "Summer")){
+        else if ((dreamVacation === "Fiji") && (favoriteSeason === "Spring" || "Summer")) {
             result = yourName + " you're a " + favoriteSeason + " person with a sense of adventure, you should start your coding journey by learning " + "C#.";
         }
-        else if ((dreamVacation = "Fiji") && (favoriteSeason = "Fall" || "Winter")){
+        else if ((dreamVacation === "Fiji") && (favoriteSeason === "Fall" || "Winter")) {
             result = yourName + " you're a " + favoriteSeason + " person with a sense of adventure, you should start your coding journey by learning " + "Python.";
+        } else {
+            result = "idk.. your weird";
         }
-
+        console.log("result: ", result);
 
         document.getElementById("output").innerText = result;
     });
