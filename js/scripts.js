03f3875391e6d46bc8798427e713640067643072
Molly Donegan <mollyrdonegan@gmail.com>
2023-01-13 10:12:03 -0800

    set up javascript mapping

--- js/scripts.js
+++ js/scripts.js
@@ -3,24 +3,18 @@ window.onload = function () {
 
     form.addEventListener("submit", function (event) {
         event.preventDefault();
-
-        const name = document.querySelector("input#name").value;
-        const dob = document.getElementById("born").value;
+    }
+        const yourName = document.querySelector("input#name").value;
+        const favoriteTime = document.getElementById("time").value;
         const favoriteColor = document.getElementById("color").value;
-        const radioSelection = document.querySelector("input[name='skills']:checked").value;
+        const dreamVacation = document.querySelector("input[name='skills']:checked").value;
+        const favSeason = document.getElementById("season").value;
 
         let result = "";
-        if (dob > "1993-01-01") {
-            result = name + " is sneaky," + " loves " + favoriteColor + ", and their special skill is " + radioSelection + ".";
-        }
-        else if (("1970-01-01" < dob) && (dob < "1992-12-31")) {
-            result = name + " is smart," + " loves " + favoriteColor + ", and their special skill is " + radioSelection + ".";
-        }
-        else if ((dob < "1969-12-31")){
-            result = name + " is brave," + " loves " + favoriteColor + ", and their special skill is " + radioSelection + ".";
+        if (dreamVacation = "Tokyo") + (favoriteTime <= "12:00PM")) {
+            result = "name, " + " you're a morning person with a sense of adventure, you should start your coding journey by learning Javascript.";
         }
 
         document.getElementById("output").innerText = result;
-    });
 
 }
